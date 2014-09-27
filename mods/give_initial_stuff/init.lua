87489e5b00546ef5205875bc9d16fcb45524f0b7
donat_b <donat@opmbx.org>
2014-09-27 07:55:38 +0400

    Update give_initial_stuff

--- mods/give_initial_stuff/init.lua
+++ mods/give_initial_stuff/init.lua
@@ -1,12 +1,16 @@
 minetest.register_on_newplayer(function(player)
 	--print("on_newplayer")
 	if minetest.setting_getbool("give_initial_stuff") then
-		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
-		player:get_inventory():add_item('main', 'default:pick_steel')
-		player:get_inventory():add_item('main', 'default:torch 99')
-		player:get_inventory():add_item('main', 'default:axe_steel')
-		player:get_inventory():add_item('main', 'default:shovel_steel')
-		player:get_inventory():add_item('main', 'default:cobble 99')
+		local name = player:get_player_name()
+		if not name:find('Guest') then
+			minetest.log("action", "Giving initial stuff to player "..name)
+			player:get_inventory():add_item('main', 'default:pick_steel')
+			player:get_inventory():add_item('main', 'default:shovel_steel')
+			player:get_inventory():add_item('main', 'default:axe_steel')
+			player:get_inventory():add_item('main', 'default:torch 10')
+			player:get_inventory():add_item('main', 'default:chest_locked')
+			player:get_inventory():add_item('main', 'farming_plus:orange_item 30')
+		end
 	end
 end)
 
