3b9bbc4cbebf77fae55962650cf540f6a1eb873b
donat_b <donat@opmbx.org>
2014-10-09 14:29:05 +0400

    Add deprecated items

--- mods/farming_plus/cotton.lua
+++ mods/farming_plus/cotton.lua
@@ -1,12 +1,24 @@
 --DEPRECATED
 
-minetest.register_craftitem('farming_plus:cotton_seed', {
+core.register_craftitem('farming_plus:cotton_seed', {
 	description = 'Cotton Seeds',
 	inventory_image = 'farming_cotton_seed.png',
 	groups = { not_in_creative_inventory=1 },
 	on_place = function(itemstack, placer, pointed_thing)
 		return farming.place_seed(itemstack, placer, pointed_thing, 'farming:seed_cotton')
 	end
+})
+
+core.register_craftitem('farming_plus:cotton', {
+	description = 'Cotton (deprecated)',
+	inventory_image = 'farming_cotton.png',
+	groups = { not_in_creative_inventory=1 },
+})
+
+core.register_craft({
+	output = "farming:cotton",
+	type = "shapeless",
+	recipe = { 'farming_plus:cotton' }
 })
 
 core.register_alias( 'farming_plus:cotton_1', 'farming:cotton_3' )
--- mods/farming_plus/wheat.lua
+++ mods/farming_plus/wheat.lua
@@ -1,12 +1,24 @@
 --DEPRECATED
 
-minetest.register_craftitem('farming_plus:wheat_seed', {
-	description = 'Wheat Seeds',
+core.register_craftitem('farming_plus:wheat_seed', {
+	description = 'Wheat Seed (deprecated)',
 	inventory_image = 'farming_wheat_seed.png',
 	groups = { not_in_creative_inventory=1 },
 	on_place = function(itemstack, placer, pointed_thing)
 		return farming.place_seed(itemstack, placer, pointed_thing, 'farming:seed_wheat')
 	end
+})
+
+core.register_craftitem('farming_plus:wheat', {
+	description = 'Wheat (deprecated)',
+	inventory_image = 'farming_wheat.png',
+	groups = { not_in_creative_inventory=1 },
+})
+
+core.register_craft({
+	output = "farming:wheat",
+	type = "shapeless",
+	recipe = { 'farming_plus:wheat' }
 })
 
 core.register_alias( 'farming_plus:wheat_1', 'farming:wheat_3' )
