6f02fbf4fb98f023b513c335967691885c3e2c5f
donat_b <donat@opmbx.org>
2014-10-12 14:54:57 +0400

    Update node definitions and fix some minor bugs (hydro)

--- mods/hydro/init.lua
+++ mods/hydro/init.lua
@@ -14,75 +14,8 @@ local PLANTS = {
 	yroses = {name='yroses', description="Roses (Yellow)", growtype='growtall',give_on_harvest='hydro:rosebush_yellow'},
 	wroses = {name='wroses', description="Roses (White)", growtype='growtall',give_on_harvest='hydro:rosebush_white'}
 }
-
-core.register_node("hydro:wine", {
-	description = 'Wine Bottle',
-	drawtype = 'nodebox',
-	tiles = {
-		'hydro_wine_top.png',
-		'hydro_bottle_wine_sides.png'
-	},
-	inventory_image = 'hydro_bottle_wine_inv.png',
-	paramtype = "light",
-	paramtype2 = "facedir",
-	groups = {
-		snappy=2,
-		choppy=2,
-		oddly_breakable_by_hand=2,
-	},
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-.022, -.12, -.022, .022, -0.1, .022}, -- wider upper
-			{-.02, -.25, -.02, .02, -0.1, .02}, -- upper
-			{-.045, -.5, -.045, .045, -.22, .045}, -- base
-		}
-	},
-	sounds = default.node_sound_glass_defaults(),
-	on_use = core.item_eat(1)
-})
-
-core.register_node("hydro:coffeecup", {
-	description = 'Coffee Cup',
-	drawtype = "plantlike",
-	tiles = {"hydro_coffeecup.png"},
-	inventory_image = "hydro_coffeecup.png",
-	wield_image = "hydro_coffeecup.png",
-	use_texture_alpha = true,
-	paramtype = "light",
-	groups = {snappy=2,choppy=2,oddly_breakable_by_hand=2,flammable=3},
-	sounds = default.node_sound_wood_defaults(),
-	walkable = false,
-	on_use = core.item_eat(2)
-})
-
-core.register_node("hydro:growlamp", {
-	description = "Growlamp",
-	drawtype = 'nodebox',
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-1/16,	-7/16,	-1/16,	1/16,	8/16,	1/16},
-			{-2/16,	3/16,	-2/16,	2/16,	6/16,	2/16},
-			{-3/16,	-6/16,	-2/16,	3/16,	3/16,	2/16},
-			{-2/16,	-6/16,	-3/16,	2/16,	3/16,	3/16},
-		}
-	},
-	tiles = {"hydro_growlamp_top.png", "hydro_growlamp_bottom.png", "hydro_growlamp_side.png"},
-	--inventory_image = "hydro_growlamp_side.png",
-	paramtype = "light",
-	light_propagates = true,
-	sunlight_propagates = true,
-	light_source = 15,
-	groups = {
-		snappy=2,
-		cracky=3,
-		oddly_breakable_by_hand=3,
-		attached_node=1
-	},
-	sounds = default.node_sound_glass_defaults(),
-	node_placement_prediction = "hydro:coffeecup",
-})
+dofile( core.get_modpath('hydro') .. '/nodes.lua' )
+dofile( core.get_modpath('hydro') .. '/crafting.lua' )
 
 core.on_place = core.on_place or function(name, func)
 	local previous_on_place = core.registered_nodes[name].on_place
@@ -114,109 +47,12 @@ core.on_place("hydro:growlamp", function(itemstack, placer, pointed_thing)
 	return true
 end)
 
-core.register_node("hydro:promix", {
-	description = "Promix",
-	tiles = {"hydro_promix.png"},
-	groups = {crumbly=3},
-	sounds = default.node_sound_dirt_defaults(),
-})
-core.register_node("hydro:roastedcoffee", {
-	description = "Roasted Coffee",
-	tiles = {"hydro_roastedcoffee.png"},
-	inventory_image = core.inventorycube("hydro_roastedcoffee.png"),
-	groups = {snappy=2,cracky=3,oddly_breakable_by_hand=3},
-	sounds = default.node_sound_stone_defaults(),
-})
-core.register_node("hydro:rosebush", {
-	description = "Rose Bush",
-	drawtype = "allfaces_optional",
-	visual_scale = 1.3,
-	tiles = {"hydro_rosebush.png"},
-	paramtype = "light",
-	groups = {
-		snappy=3,
-		flammable=2,
-		not_in_creative_inventory=1
-	},
-	sounds = default.node_sound_leaves_defaults(),
-})
-
-core.register_node("hydro:rosebush_white", {
-	description = "Rose Bush (White)",
-	drawtype = "allfaces_optional",
-	visual_scale = 1.3,
-	tiles = {"hydro_rosebush_white.png"},
-	paramtype = "light",
-	groups = {
-		snappy=3,
-		flammable=2,
-		not_in_creative_inventory=1
-	},
-	sounds = default.node_sound_leaves_defaults(),
-})
-
-core.register_node("hydro:rosebush_yellow", {
-	description = "Rose Bush (Yellow)",
-	drawtype = "allfaces_optional",
-	visual_scale = 1.3,
-	tiles = {"hydro_rosebush_yellow.png"},
-	paramtype = "light",
-	groups = {
-		snappy=3,
-		flammable=2,
-		not_in_creative_inventory=1
-	},
-	sounds = default.node_sound_leaves_defaults(),
-})
-
-core.register_node("hydro:rosebush_violet", {
-	description = "Rose Bush (Violet)",
-	drawtype = "allfaces_optional",
-	visual_scale = 1.3,
-	tiles = {"hydro_rosebush_violet.png"},
-	paramtype = "light",
-	groups = {
-		snappy=3,
-		flammable=2,
-		not_in_creative_inventory=1
-	},
-	sounds = default.node_sound_leaves_defaults(),
-})
-
-core.register_node("hydro:rosebush_blue", {
-	description = "Rose Bush (Blue)",
-	drawtype = "allfaces_optional",
-	visual_scale = 1.3,
-	tiles = {"hydro_rosebush_blue.png"},
-	paramtype = "light",
-	groups = {
-		snappy=3,
-		flammable=2,
-		not_in_creative_inventory=1
-	},
-	sounds = default.node_sound_leaves_defaults(),
-})
-
-core.register_node("hydro:rosebush_pink", {
-	description = "Rose Bush (Pink)",
-	drawtype = "allfaces_optional",
-	visual_scale = 1.3,
-	tiles = {"hydro_rosebush_pink.png"},
-	paramtype = "light",
-	groups = {
-		snappy=3,
-		flammable=2,
-		not_in_creative_inventory=1
-	},
-	sounds = default.node_sound_leaves_defaults(),
-})
-
 local get_plantname = {}		-- plants index by nodenames (tomato1, tomato2, seeds_tomato, etc..)
 local get_plantbynumber = {}		-- plants index by number (for random select)
 local get_wildplants = {}		-- wildplant nodenames (pop control)
 
 for _,plant in pairs(PLANTS) do 
-		--		define nodes
+	-- define nodes
 	local wild_plant = "hydro:wild_"..plant.name
 	core.register_node(wild_plant, {
 		description = "Wild "..plant.description.." Plant",
@@ -508,6 +344,5 @@ core.register_abm({
 		end
 	end
 })
-dofile( core.get_modpath('hydro') .. '/crafting.lua' )
 
 print(string.format('[hydro] loaded after ca. %.3fs', os.clock() - load_time_start))
