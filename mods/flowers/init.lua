minetest/minetest_game

Add rotation to plants.

mods/flowers/init.lua
@@ -130,7 +130,8 @@ minetest.register_abm({
 		local under = minetest.get_node(pos)
 		pos.y = pos.y + 1
 		if under.name == "default:desert_sand" then
-			minetest.set_node(pos, {name="default:dry_shrub"})
+			minetest.set_node(pos,
+				{name = "default:dry_shrub", param2 = math.random(0, 179)})
 		elseif under.name ~= "default:dirt_with_grass" then
 			return
 		end

ayntest/ayntest_game

mods/flowers/init.lua
@@ -132,7 +132,8 @@ core.register_abm({
 		local under = core.get_node(pos)
 		pos.y = pos.y + 1
 		if under.name == "default:desert_sand" then
-			core.set_node(pos, {name="default:dry_shrub"})
+			core.set_node(pos,
+				{name = "default:dry_shrub", param2 = math.random(0, 179)})
 		elseif under.name ~= "default:dirt_with_grass" then
 			return
 		end