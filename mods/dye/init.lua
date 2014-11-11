4b89c525672cb3004060931b08bf418d396afdd7
donat_b <donat@opmbx.org>
2014-11-11 15:36:09 +0300

    Revert "Disable broken recipe (dyes)"
    This reverts commit e13b0289e1bb319dfb404a63587cb8a6c40cfcf8.

--- mods/dye/init.lua
+++ mods/dye/init.lua
@@ -92,11 +92,11 @@ for _, row in ipairs(dyelocal.dyes) do
 		description = description,
 		groups = groups
 	})
-	--[[minetest.register_craft({
+	minetest.register_craft({
 		type = "shapeless",
 		output = item_name.." 4",
 		recipe = {"group:flower,color_"..name},
-	})]]
+	})
 end
 -- manually add coal->black dye
 minetest.register_craft({
