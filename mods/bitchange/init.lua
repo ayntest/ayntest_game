ab02010da1e5baf7e7b081aa60c48d6bcbba8951
donat_b <donat@opmbx.org>
2014-12-08 19:56:14 +0300

    chg: renames

--- mods/bitchange/init.lua
+++ mods/bitchange/init.lua
@@ -1,9 +1,9 @@
 --Created by Krock for the BitChange mod
-local mod_path = minetest.get_modpath("bitchange")
-local world_path = minetest.get_worldpath()
+local mod_path = core.get_modpath("bitchange")
+local world_path = core.get_worldpath()
 
 if freeminer then
-	minetest = freeminer
+	core.= freeminer
 end
 
 dofile(mod_path.."/config.default.txt")
@@ -47,13 +47,13 @@ end
 --[[
 if(bitchange_enable_bank) then
 	local loaded_bank = ""
-	if(minetest.get_modpath("money") ~= nil) then
+	if(core.get_modpath("money") ~= nil) then
 		loaded_bank = "money"
 		dofile(mod_path.."/bank_"..loaded_bank..".lua")
-	elseif(minetest.get_modpath("money2") ~= nil) then
+	elseif(core.get_modpath("money2") ~= nil) then
 		loaded_bank = "money2"
 		dofile(mod_path.."/bank_"..loaded_bank..".lua")
-	elseif(minetest.get_modpath("currency") ~= nil) then
+	elseif(core.get_modpath("currency") ~= nil) then
 		loaded_bank = "currency"
 		dofile(mod_path.."/bank_"..loaded_bank..".lua")
 	end
@@ -62,15 +62,15 @@ if(bitchange_enable_bank) then
 	end
 end
 
-if(not minetest.setting_getbool("creative_mode") and bitchange_initial_give > 0) then
+if(not core.setting_getbool("creative_mode") and bitchange_initial_give > 0) then
 	-- Giving initial money
-	minetest.register_on_newplayer(function(player)
+	core.register_on_newplayer(function(player)
 		player:get_inventory():add_item("main", "bitchange:mineninth "..bitchange_initial_give)
 	end)
 end
 ]]--
 -- Privs
-minetest.register_privilege("bitchange", "Can access to owned nodes of the bitchange mod")
+core.register_privilege("bitchange", "Can access to owned nodes of the bitchange mod")
 
 function bitchange_has_access(owner, player_name)
 	return (player_name == owner or owner == "" or core.check_player_privs( name, { access=true } ) == true)
